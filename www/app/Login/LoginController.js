336e395ab13e4c865d95c7ddb22fe7978a8ef960
iamsim <freelancer.ajith@gmail.com>
2018-08-07 01:26:03 +0530

    *** New push to play store [07/08/2018] ***

--- www/app/Login/LoginController.js
+++ www/app/Login/LoginController.js
@@ -3,9 +3,9 @@
 angular.module('officeTimerApp').controller('LoginController', function($scope, $state, LoginFactory, ionicToast) {
 
     $scope.loginData = {
-        base_url: "https://next.officetimer.com",
-        username: "kalyan713@co.com123",
-        password: "welcome1#"
+        base_url: "https://prod.officetimer.com",
+        username: "",
+        password: ""
     };
 
     $scope.errorMessage = null;
